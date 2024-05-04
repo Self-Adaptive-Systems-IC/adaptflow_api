80e5f015c0e52e478e6492a8823596714a595b69
Rômulo Soares <romulosoares@ice.ufjf.br>
2024-05-03 22:25:13 -0300

    Update main

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 import uvicorn
-from dotenv import dotenv_values
+from dotenv import dotenv_values,load_dotenv
 import os
-config = dotenv_values(".env")
+load_dotenv()
+# print(os.environ.get("IP"))
+IP = os.getenv('IP', '0.0.0.0')
+PORT = os.getenv('PORT', '99')
+
+
 def create_directory_if_not_exists(directory_path):
     if not os.path.exists(directory_path):
         os.makedirs(directory_path)
         print(f"Directory '{directory_path}' created successfully.")
     else:
         print(f"Directory '{directory_path}' already exists.")
-print(config)
-config = {
-"IP":"0.0.0.0",
-"PORT":"82"
-}
+
+
 create_directory_if_not_exists("./tmp")
 create_directory_if_not_exists("./tmp/files")
 create_directory_if_not_exists("./tmp/loaded_models")
-host = str(config["IP"])
-port = int(config["PORT"])
-print(host, port)
-uvicorn.run("src.app:app", host=host, port=port)
+
+print(f"{str(IP)}:{int(PORT)}")
+uvicorn.run("src.app:app", host=str(IP), port=int(PORT))
 
 
     
